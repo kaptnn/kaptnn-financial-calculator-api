f477d488380d43e562004afa8007e5e480f1de21
Rizalianmi <rizalianmi.tambunan@gmail.com>
2025-01-15 14:30:54 +0700

    menambahkan fitur pengali

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -3,9 +3,10 @@ from app.routes.endpoints.calculator import router as calculator_router
 from app.routes.endpoints.goal_seeking import router as goal_seeking_router
 from app.routes.endpoints.auth import router as auth_router
 from app.routes.endpoints.user import router as user_router
+from app.routes.endpoints.pengali import router as pengali_router
 
 routers = APIRouter()
-router_list = [auth_router, user_router, calculator_router, goal_seeking_router]
+router_list = [auth_router, user_router, calculator_router, goal_seeking_router, pengali_router]
 
 for router in router_list:
     routers.include_router(router)
