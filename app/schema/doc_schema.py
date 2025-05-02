909a02b1c376a880d81747a93c303a70b4b9036c
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-05-02 14:05:39 +0700

    fix(docs): fix document schema

--- app/schema/doc_schema.py
+++ app/schema/doc_schema.py
@@ -1,41 +1,50 @@
-import uuid
+from uuid import UUID
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Union
 from pydantic import BaseModel
-from app.schema.base_schema import ModelBaseInfo
+from app.schema.base_schema import FindBase, ModelBaseInfo
 from app.utils.schema import AllOptional
 
 class BaseDocument(BaseModel):
-    request_id: str
-    uploaded_by: str
-    company_id: str
+    request_id: UUID
+    uploaded_by: UUID
+    company_id: UUID
     document_name: str 
     document_path: str 
     file_size: int 
     mime_type: str  
+
+    class Config:
+        from_attributes: True
+
+class Document(ModelBaseInfo, BaseDocument, metaclass=AllOptional): 
     created_at: Optional[datetime]
     updated_at: Optional[datetime]
 
-class Document(ModelBaseInfo, BaseDocument, metaclass=AllOptional): ...
+    class Config:
+        from_attributes: True
 
 class FindAllDocumentsResponse(BaseModel):
     message: str
     result: Optional[List[Document]]
+    meta: Optional[FindBase]
 
 class FindDocumentByOptionsRequest(BaseModel):
     option: str
-    value: str | int | uuid.UUID
+    value: Optional[Union[str, UUID]]
 
 class FindDocumentByOptionsResponse(BaseModel):
     message: str
     result: Optional[Document]
+    meta: Optional[FindBase]
 
 class CreateDocumentRequest(BaseModel):
     document_name: str 
 
 class CreateDocumentResponse(BaseModel):
     message: str
-    result: Document
+    result: Optional[Document]
+    meta: Optional[FindBase]
 
 class UpdateDocumentRequest(BaseModel):
     document_name: str   
@@ -43,6 +52,9 @@ class UpdateDocumentRequest(BaseModel):
 class UpdateDocumentResponse(BaseModel):
     message: str
     result: Optional[Document]  
+    meta: Optional[FindBase]
 
 class DeleteDocumentResponse(BaseModel):
     message: str
+    result: Optional[Document]
+    meta: Optional[FindBase]
